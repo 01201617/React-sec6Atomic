dfcdce19f519629189a143e9a5b3b4fd93a61ec3
Nan <40654091+01201617@users.noreply.github.com>
2021-11-11 17:34:00 +0900

    Atomicボタン作成(styled-componentsを用いて) React-sec6Atomic

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
+import { BaseButton } from "./components/button/BaseButton";
+import { PrimaryButton } from "./components/button/PrimaryButton";
+import { SecondaryButton } from "./components/button/SecondaryButton";
 import "./styles.css";
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <PrimaryButton>ボタン</PrimaryButton>
+      <SecondaryButton>2ndボタン</SecondaryButton>
     </div>
   );
 }
